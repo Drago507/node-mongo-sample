9edae7b91bc6c2010467be4af8e4a556fc466e63
Amir Maralani <maralani@kpp.ir>
2017-12-20 18:38:31 +0330

    some new methods

--- app/routes/bot_routes.js
+++ app/routes/bot_routes.js
@@ -2,19 +2,8 @@ var ObjectID = require('mongodb').ObjectID;
 
 module.exports = {
     addMessage(ctx, db) {
-        const message = {
-            message_id: ctx.message.message_id,
-            fromId: ctx.message.from.id,
-            date: ctx.message.date,
-            chatId: ctx.message.chat.id,
-            chatType: ctx.message.chat.type,
-            userLeftChat: false,
-            createdAt: new Date(),
-            updatedAt: new Date()
-        };
-        db.collection('messages').insertOne(message, (err, result) => {
-
-        });
+        saveUserNewName(ctx, db);
+        saveMessage(ctx,db);
     },
     getTop(ctx, db) {
         db.collection('messages').aggregate([
@@ -26,8 +15,51 @@ module.exports = {
                     }
                 }
             ]
-        ).toArray((err,result)=>{
-            console.log(result);
+        ).toArray((err, results) => {
+            if(!err){
+                let resultText = "";
+                results.forEach(function(result){
+                    let user = getUserByUserId(result.fromId);
+                    resultText = resultText + user.firstName + " " + user.lastName + " : " + result.count;
+                });
+                ctx.reply(resultText);
+            }
+
         });
     }
 };
+
+function saveUserNewName(ctx, db) {
+    db.collection('names').updateOne(
+        {"userId": ctx.message.from.id},
+        {
+            "firstName": ctx.message.from.first_name,
+            "lastName": ctx.message.from.last_name,
+            "username": ctx.message.from.username
+        },
+        (err, result) => {
+
+        });
+}
+
+function saveMessage(ctx, db) {
+    const message = {
+        message_id: ctx.message.message_id,
+        fromId: ctx.message.from.id,
+        date: ctx.message.date,
+        chatId: ctx.message.chat.id,
+        chatType: ctx.message.chat.type,
+        userLeftChat: false,
+        createdAt: new Date(),
+        updatedAt: new Date()
+    };
+    db.collection('messages').insertOne(message, (err, result) => {
+
+    });
+}
+
+function getUserByUserId(userId,db){
+    db.collection('names').findOne({"userId":userId}).then((item)=>{
+        return item;
+    })
+}
